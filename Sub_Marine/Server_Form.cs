0e9b67d9a89182bdbb9dfa3590100f177f0d3bb9
amitaibar <amitaibar@0286fb24-2438-0410-92b8-11115a6f368b>
2007-08-26 21:04:02 +0000

    No gui hang!!

--- Sub_Marine/Server_Form.cs
+++ Sub_Marine/Server_Form.cs
@@ -5,12 +5,14 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Sub_Marine_Server
 {
     public partial class Server_Form : Form
     {
         public GameServer server;
+        private Thread init;
         public Server_Form()
         {
             InitializeComponent();
@@ -23,12 +25,14 @@ namespace Sub_Marine_Server
             int i_port = int.Parse(port.Text);
             server = new GameServer(ip.Text,i_port);
             server.m_Command = data_handler;
-            server.start();
+            init = new Thread(new ThreadStart(server.start));// server.start();
+            init.Start();
         }
         public void data_handler(String str)
         {
-            MessageBox.Show(str);
-            input.Text = str;
+            //MessageBox.Show(str);
+
+            input.Set = str;
         }
 
     }
