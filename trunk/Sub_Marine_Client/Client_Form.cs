84107f5ccf4f9861a64ba4c59cb5b183ad0a79a2
damagedspline <damagedspline@0286fb24-2438-0410-92b8-11115a6f368b>
2007-09-06 19:03:27 +0000

    fix svn

--- trunk/Sub_Marine_Client/Client_Form.cs
+++ trunk/Sub_Marine_Client/Client_Form.cs
@@ -5,11 +5,13 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Sub_Marine_Client
 {
     public partial class Client_Form : Form
     {
+    	private Thread init = null;
         private GameClient game; 
         public Client_Form()
         {
@@ -20,11 +22,19 @@ namespace Sub_Marine_Client
         private void Connect_Click(object sender, EventArgs e)
         {
             game = new GameClient(ip.Text, int.Parse(port.Text));
+            game.r_Command = reaciveEvent;
+            init = new Thread(new ThreadStart(game.start));
+            init.Start();
         }
 
         private void send_Click(object sender, EventArgs e)
         {
                     game.SendData(outData.Text);
+        }
+        
+        private void reaciveEvent(string str)
+        {
+        	input.Text = str;
         }
 
     }
