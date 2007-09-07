841c00a2529ebf0e967466411354cf986cc1c3b7
damagedspline <damagedspline@0286fb24-2438-0410-92b8-11115a6f368b>
2007-09-07 23:02:00 +0000

    fixed drag and drop. working now.

--- trunk/Sub_Marine_Client/GameGridPanel.cs
+++ trunk/Sub_Marine_Client/GameGridPanel.cs
@@ -8,11 +8,13 @@ using System.Windows.Forms;
 
 namespace Sub_Marine_Client
 {
-    public partial class GameGridPanel : UserControl
+    public partial class GameGridPanel : GridPanelBase
     {
         public GameGridPanel()
+        	:base(100)
         {
             InitializeComponent();
+            addTilesToList(Controls);
         }
     }
 }
--- trunk/Sub_Marine_Client/RepositoryGridPanel.Designer.cs
+++ trunk/Sub_Marine_Client/RepositoryGridPanel.Designer.cs
@@ -28,133 +28,263 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tile1 = new Sub_Marine_Client.Tile();
-            this.tile2 = new Sub_Marine_Client.Tile();
-            this.tile3 = new Sub_Marine_Client.Tile();
-            this.tile4 = new Sub_Marine_Client.Tile();
-            this.tile5 = new Sub_Marine_Client.Tile();
-            this.tile6 = new Sub_Marine_Client.Tile();
-            this.tile7 = new Sub_Marine_Client.Tile();
-            this.tile8 = new Sub_Marine_Client.Tile();
-            this.tile9 = new Sub_Marine_Client.Tile();
-            this.tile10 = new Sub_Marine_Client.Tile();
-            this.SuspendLayout();
-            // 
-            // tile1
-            // 
-            this.tile1.AllowDrop = true;
-            this.tile1.BackColor = System.Drawing.Color.Black;
-            this.tile1.Location = new System.Drawing.Point(3, 4);
-            this.tile1.Name = "tile1";
-            this.tile1.Size = new System.Drawing.Size(30, 30);
-            this.tile1.TabIndex = 0;
-            // 
-            // tile2
-            // 
-            this.tile2.AllowDrop = true;
-            this.tile2.BackColor = System.Drawing.Color.Black;
-            this.tile2.Cursor = System.Windows.Forms.Cursors.Default;
-            this.tile2.Location = new System.Drawing.Point(34, 4);
-            this.tile2.Name = "tile2";
-            this.tile2.Size = new System.Drawing.Size(30, 30);
-            this.tile2.TabIndex = 1;
-            // 
-            // tile3
-            // 
-            this.tile3.AllowDrop = true;
-            this.tile3.BackColor = System.Drawing.Color.Black;
-            this.tile3.Cursor = System.Windows.Forms.Cursors.Default;
-            this.tile3.Location = new System.Drawing.Point(65, 4);
-            this.tile3.Name = "tile3";
-            this.tile3.Size = new System.Drawing.Size(30, 30);
-            this.tile3.TabIndex = 2;
-            // 
-            // tile4
-            // 
-            this.tile4.AllowDrop = true;
-            this.tile4.BackColor = System.Drawing.Color.Black;
-            this.tile4.Cursor = System.Windows.Forms.Cursors.Default;
-            this.tile4.Location = new System.Drawing.Point(96, 4);
-            this.tile4.Name = "tile4";
-            this.tile4.Size = new System.Drawing.Size(30, 30);
-            this.tile4.TabIndex = 3;
-            // 
-            // tile5
-            // 
-            this.tile5.AllowDrop = true;
-            this.tile5.BackColor = System.Drawing.Color.Black;
-            this.tile5.Cursor = System.Windows.Forms.Cursors.Default;
-            this.tile5.Location = new System.Drawing.Point(128, 4);
-            this.tile5.Name = "tile5";
-            this.tile5.Size = new System.Drawing.Size(60, 30);
-            this.tile5.TabIndex = 4;
-            // 
-            // tile6
-            // 
-            this.tile6.AllowDrop = true;
-            this.tile6.BackColor = System.Drawing.Color.Black;
-            this.tile6.Cursor = System.Windows.Forms.Cursors.Default;
-            this.tile6.Location = new System.Drawing.Point(190, 4);
-            this.tile6.Name = "tile6";
-            this.tile6.Size = new System.Drawing.Size(60, 30);
-            this.tile6.TabIndex = 5;
-            // 
-            // tile7
-            // 
-            this.tile7.AllowDrop = true;
-            this.tile7.BackColor = System.Drawing.Color.Black;
-            this.tile7.Location = new System.Drawing.Point(252, 4);
-            this.tile7.Name = "tile7";
-            this.tile7.Size = new System.Drawing.Size(60, 30);
-            this.tile7.TabIndex = 6;
-            // 
-            // tile8
-            // 
-            this.tile8.AllowDrop = true;
-            this.tile8.BackColor = System.Drawing.Color.Black;
-            this.tile8.Location = new System.Drawing.Point(3, 35);
-            this.tile8.Name = "tile8";
-            this.tile8.Size = new System.Drawing.Size(90, 30);
-            this.tile8.TabIndex = 7;
-            // 
-            // tile9
-            // 
-            this.tile9.AllowDrop = true;
-            this.tile9.BackColor = System.Drawing.Color.Black;
-            this.tile9.Cursor = System.Windows.Forms.Cursors.Default;
-            this.tile9.Location = new System.Drawing.Point(96, 35);
-            this.tile9.Name = "tile9";
-            this.tile9.Size = new System.Drawing.Size(90, 30);
-            this.tile9.TabIndex = 8;
-            // 
-            // tile10
-            // 
-            this.tile10.AllowDrop = true;
-            this.tile10.BackColor = System.Drawing.Color.Black;
-            this.tile10.Location = new System.Drawing.Point(190, 35);
-            this.tile10.Name = "tile10";
-            this.tile10.Size = new System.Drawing.Size(120, 30);
-            this.tile10.TabIndex = 9;
-            // 
-            // RepositoryGridPanel
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tile10);
-            this.Controls.Add(this.tile9);
-            this.Controls.Add(this.tile8);
-            this.Controls.Add(this.tile7);
-            this.Controls.Add(this.tile6);
-            this.Controls.Add(this.tile5);
-            this.Controls.Add(this.tile4);
-            this.Controls.Add(this.tile3);
-            this.Controls.Add(this.tile2);
-            this.Controls.Add(this.tile1);
-            this.Name = "RepositoryGridPanel";
-            this.Size = new System.Drawing.Size(318, 65);
-            this.ResumeLayout(false);
-
+        	this.tile1 = new Sub_Marine_Client.Tile(this);
+        	this.tile2 = new Sub_Marine_Client.Tile(this);
+        	this.tile3 = new Sub_Marine_Client.Tile(this);
+        	this.tile4 = new Sub_Marine_Client.Tile(this);
+        	this.tile5 = new Sub_Marine_Client.Tile(this);
+        	this.tile6 = new Sub_Marine_Client.Tile(this);
+        	this.tile7 = new Sub_Marine_Client.Tile(this);
+        	this.tile8 = new Sub_Marine_Client.Tile(this);
+        	this.tile9 = new Sub_Marine_Client.Tile(this);
+        	this.tile10 = new Sub_Marine_Client.Tile(this);
+        	this.tile11 = new Sub_Marine_Client.Tile(this);
+        	this.tile12 = new Sub_Marine_Client.Tile(this);
+        	this.tile13 = new Sub_Marine_Client.Tile(this);
+        	this.tile14 = new Sub_Marine_Client.Tile(this);
+        	this.tile15 = new Sub_Marine_Client.Tile(this);
+        	this.tile16 = new Sub_Marine_Client.Tile(this);
+        	this.tile17 = new Sub_Marine_Client.Tile(this);
+        	this.tile18 = new Sub_Marine_Client.Tile(this);
+        	this.tile19 = new Sub_Marine_Client.Tile(this);
+        	this.tile20 = new Sub_Marine_Client.Tile(this);
+        	this.SuspendLayout();
+        	// 
+        	// tile1
+        	// 
+        	this.tile1.AllowDrop = true;
+        	this.tile1.BackColor = System.Drawing.Color.Black;
+        	this.tile1.Location = new System.Drawing.Point(3, 5);
+        	this.tile1.Name = "tile1";
+        	this.tile1.Size = new System.Drawing.Size(30, 30);
+        	this.tile1.TabIndex = 0;
+        	// 
+        	// tile2
+        	// 
+        	this.tile2.AllowDrop = true;
+        	this.tile2.BackColor = System.Drawing.Color.Black;
+        	this.tile2.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile2.Location = new System.Drawing.Point(34, 5);
+        	this.tile2.Name = "tile2";
+        	this.tile2.Size = new System.Drawing.Size(30, 30);
+        	this.tile2.TabIndex = 1;
+        	// 
+        	// tile3
+        	// 
+        	this.tile3.AllowDrop = true;
+        	this.tile3.BackColor = System.Drawing.Color.Black;
+        	this.tile3.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile3.Location = new System.Drawing.Point(65, 5);
+        	this.tile3.Name = "tile3";
+        	this.tile3.Size = new System.Drawing.Size(30, 30);
+        	this.tile3.TabIndex = 2;
+        	// 
+        	// tile4
+        	// 
+        	this.tile4.AllowDrop = true;
+        	this.tile4.BackColor = System.Drawing.Color.Black;
+        	this.tile4.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile4.Location = new System.Drawing.Point(96, 5);
+        	this.tile4.Name = "tile4";
+        	this.tile4.Size = new System.Drawing.Size(30, 30);
+        	this.tile4.TabIndex = 3;
+        	// 
+        	// tile5
+        	// 
+        	this.tile5.AllowDrop = true;
+        	this.tile5.BackColor = System.Drawing.Color.Black;
+        	this.tile5.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile5.Location = new System.Drawing.Point(127, 5);
+        	this.tile5.Name = "tile5";
+        	this.tile5.Size = new System.Drawing.Size(30, 30);
+        	this.tile5.TabIndex = 4;
+        	// 
+        	// tile6
+        	// 
+        	this.tile6.AllowDrop = true;
+        	this.tile6.BackColor = System.Drawing.Color.Black;
+        	this.tile6.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile6.Location = new System.Drawing.Point(158, 5);
+        	this.tile6.Name = "tile6";
+        	this.tile6.Size = new System.Drawing.Size(30, 30);
+        	this.tile6.TabIndex = 5;
+        	// 
+        	// tile7
+        	// 
+        	this.tile7.AllowDrop = true;
+        	this.tile7.BackColor = System.Drawing.Color.Black;
+        	this.tile7.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile7.Location = new System.Drawing.Point(189, 5);
+        	this.tile7.Name = "tile7";
+        	this.tile7.Size = new System.Drawing.Size(30, 30);
+        	this.tile7.TabIndex = 6;
+        	// 
+        	// tile8
+        	// 
+        	this.tile8.AllowDrop = true;
+        	this.tile8.BackColor = System.Drawing.Color.Black;
+        	this.tile8.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile8.Location = new System.Drawing.Point(220, 5);
+        	this.tile8.Name = "tile8";
+        	this.tile8.Size = new System.Drawing.Size(30, 30);
+        	this.tile8.TabIndex = 7;
+        	// 
+        	// tile9
+        	// 
+        	this.tile9.AllowDrop = true;
+        	this.tile9.BackColor = System.Drawing.Color.Black;
+        	this.tile9.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile9.Location = new System.Drawing.Point(251, 5);
+        	this.tile9.Name = "tile9";
+        	this.tile9.Size = new System.Drawing.Size(30, 30);
+        	this.tile9.TabIndex = 8;
+        	// 
+        	// tile10
+        	// 
+        	this.tile10.AllowDrop = true;
+        	this.tile10.BackColor = System.Drawing.Color.Black;
+        	this.tile10.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile10.Location = new System.Drawing.Point(282, 5);
+        	this.tile10.Name = "tile10";
+        	this.tile10.Size = new System.Drawing.Size(30, 30);
+        	this.tile10.TabIndex = 9;
+        	// 
+        	// tile11
+        	// 
+        	this.tile11.AllowDrop = true;
+        	this.tile11.BackColor = System.Drawing.Color.Black;
+        	this.tile11.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile11.Location = new System.Drawing.Point(3, 36);
+        	this.tile11.Name = "tile11";
+        	this.tile11.Size = new System.Drawing.Size(30, 30);
+        	this.tile11.TabIndex = 19;
+        	// 
+        	// tile12
+        	// 
+        	this.tile12.AllowDrop = true;
+        	this.tile12.BackColor = System.Drawing.Color.Black;
+        	this.tile12.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile12.Location = new System.Drawing.Point(34, 36);
+        	this.tile12.Name = "tile12";
+        	this.tile12.Size = new System.Drawing.Size(30, 30);
+        	this.tile12.TabIndex = 18;
+        	// 
+        	// tile13
+        	// 
+        	this.tile13.AllowDrop = true;
+        	this.tile13.BackColor = System.Drawing.Color.Black;
+        	this.tile13.Location = new System.Drawing.Point(65, 36);
+        	this.tile13.Name = "tile13";
+        	this.tile13.Size = new System.Drawing.Size(30, 30);
+        	this.tile13.TabIndex = 17;
+        	// 
+        	// tile14
+        	// 
+        	this.tile14.AllowDrop = true;
+        	this.tile14.BackColor = System.Drawing.Color.Black;
+        	this.tile14.Location = new System.Drawing.Point(96, 36);
+        	this.tile14.Name = "tile14";
+        	this.tile14.Size = new System.Drawing.Size(30, 30);
+        	this.tile14.TabIndex = 16;
+        	// 
+        	// tile15
+        	// 
+        	this.tile15.AllowDrop = true;
+        	this.tile15.BackColor = System.Drawing.Color.Black;
+        	this.tile15.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile15.Location = new System.Drawing.Point(127, 36);
+        	this.tile15.Name = "tile15";
+        	this.tile15.Size = new System.Drawing.Size(30, 30);
+        	this.tile15.TabIndex = 15;
+        	// 
+        	// tile16
+        	// 
+        	this.tile16.AllowDrop = true;
+        	this.tile16.BackColor = System.Drawing.Color.Black;
+        	this.tile16.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile16.Location = new System.Drawing.Point(158, 36);
+        	this.tile16.Name = "tile16";
+        	this.tile16.Size = new System.Drawing.Size(30, 30);
+        	this.tile16.TabIndex = 14;
+        	// 
+        	// tile17
+        	// 
+        	this.tile17.AllowDrop = true;
+        	this.tile17.BackColor = System.Drawing.Color.Black;
+        	this.tile17.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile17.Location = new System.Drawing.Point(189, 36);
+        	this.tile17.Name = "tile17";
+        	this.tile17.Size = new System.Drawing.Size(30, 30);
+        	this.tile17.TabIndex = 13;
+        	// 
+        	// tile18
+        	// 
+        	this.tile18.AllowDrop = true;
+        	this.tile18.BackColor = System.Drawing.Color.Black;
+        	this.tile18.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile18.Location = new System.Drawing.Point(220, 36);
+        	this.tile18.Name = "tile18";
+        	this.tile18.Size = new System.Drawing.Size(30, 30);
+        	this.tile18.TabIndex = 12;
+        	// 
+        	// tile19
+        	// 
+        	this.tile19.AllowDrop = true;
+        	this.tile19.BackColor = System.Drawing.Color.Black;
+        	this.tile19.Cursor = System.Windows.Forms.Cursors.Default;
+        	this.tile19.Location = new System.Drawing.Point(251, 36);
+        	this.tile19.Name = "tile19";
+        	this.tile19.Size = new System.Drawing.Size(30, 30);
+        	this.tile19.TabIndex = 11;
+        	// 
+        	// tile20
+        	// 
+        	this.tile20.AllowDrop = true;
+        	this.tile20.BackColor = System.Drawing.Color.Black;
+        	this.tile20.Location = new System.Drawing.Point(282, 36);
+        	this.tile20.Name = "tile20";
+        	this.tile20.Size = new System.Drawing.Size(30, 30);
+        	this.tile20.TabIndex = 10;
+        	// 
+        	// RepositoryGridPanel
+        	// 
+        	this.AllowDrop = true;
+        	this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        	this.Controls.Add(this.tile11);
+        	this.Controls.Add(this.tile12);
+        	this.Controls.Add(this.tile13);
+        	this.Controls.Add(this.tile14);
+        	this.Controls.Add(this.tile15);
+        	this.Controls.Add(this.tile16);
+        	this.Controls.Add(this.tile17);
+        	this.Controls.Add(this.tile18);
+        	this.Controls.Add(this.tile19);
+        	this.Controls.Add(this.tile20);
+        	this.Controls.Add(this.tile10);
+        	this.Controls.Add(this.tile9);
+        	this.Controls.Add(this.tile8);
+        	this.Controls.Add(this.tile7);
+        	this.Controls.Add(this.tile6);
+        	this.Controls.Add(this.tile5);
+        	this.Controls.Add(this.tile4);
+        	this.Controls.Add(this.tile3);
+        	this.Controls.Add(this.tile2);
+        	this.Controls.Add(this.tile1);
+        	this.Name = "RepositoryGridPanel";
+        	this.Size = new System.Drawing.Size(317, 72);
+        	this.ResumeLayout(false);
         }
+        private Sub_Marine_Client.Tile tile20;
+        private Sub_Marine_Client.Tile tile19;
+        private Sub_Marine_Client.Tile tile18;
+        private Sub_Marine_Client.Tile tile17;
+        private Sub_Marine_Client.Tile tile16;
+        private Sub_Marine_Client.Tile tile15;
+        private Sub_Marine_Client.Tile tile14;
+        private Sub_Marine_Client.Tile tile13;
+        private Sub_Marine_Client.Tile tile12;
+        private Sub_Marine_Client.Tile tile11;
 
         #endregion
 
