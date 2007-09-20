d27604e4160ea21fdd644e0267e5222637d85255
damagedspline <damagedspline@0286fb24-2438-0410-92b8-11115a6f368b>
2007-09-20 10:59:44 +0000

    Added logger to server. logger is using a delegate.

--- Sub_Marine/Server_Form.Designer.cs
+++ Sub_Marine/Server_Form.Designer.cs
@@ -28,23 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
-        	this.input = new System.Windows.Forms.TextBox();
         	this.lunchServer = new System.Windows.Forms.Button();
-        	this.label1 = new System.Windows.Forms.Label();
         	this.port = new System.Windows.Forms.TextBox();
         	this.label3 = new System.Windows.Forms.Label();
+        	this.m_loggerOutput = new System.Windows.Forms.ListBox();
         	this.SuspendLayout();
-        	// 
-        	// input
-        	// 
-        	this.input.Location = new System.Drawing.Point(90, 28);
-        	this.input.Name = "input";
-        	this.input.Size = new System.Drawing.Size(86, 20);
-        	this.input.TabIndex = 0;
         	// 
         	// lunchServer
         	// 
-        	this.lunchServer.Location = new System.Drawing.Point(182, 28);
+        	this.lunchServer.Location = new System.Drawing.Point(170, 7);
         	this.lunchServer.Name = "lunchServer";
         	this.lunchServer.Size = new System.Drawing.Size(50, 41);
         	this.lunchServer.TabIndex = 1;
@@ -52,20 +44,11 @@
         	this.lunchServer.UseVisualStyleBackColor = true;
         	this.lunchServer.Click += new System.EventHandler(this.lunchServer_Click);
         	// 
-        	// label1
-        	// 
-        	this.label1.AutoSize = true;
-        	this.label1.Location = new System.Drawing.Point(135, 9);
-        	this.label1.Name = "label1";
-        	this.label1.Size = new System.Drawing.Size(31, 13);
-        	this.label1.TabIndex = 2;
-        	this.label1.Text = "Input";
-        	// 
         	// port
         	// 
         	this.port.Location = new System.Drawing.Point(13, 28);
         	this.port.Name = "port";
-        	this.port.Size = new System.Drawing.Size(56, 20);
+        	this.port.Size = new System.Drawing.Size(138, 20);
         	this.port.TabIndex = 4;
         	// 
         	// label3
@@ -77,28 +60,35 @@
         	this.label3.TabIndex = 6;
         	this.label3.Text = "port";
         	// 
+        	// m_loggerOutput
+        	// 
+        	this.m_loggerOutput.FormattingEnabled = true;
+        	this.m_loggerOutput.HorizontalScrollbar = true;
+        	this.m_loggerOutput.Location = new System.Drawing.Point(12, 72);
+        	this.m_loggerOutput.Name = "m_loggerOutput";
+        	this.m_loggerOutput.Size = new System.Drawing.Size(348, 186);
+        	this.m_loggerOutput.TabIndex = 7;
+        	// 
         	// Server_Form
         	// 
         	this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
         	this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        	this.ClientSize = new System.Drawing.Size(232, 119);
+        	this.ClientSize = new System.Drawing.Size(372, 276);
+        	this.Controls.Add(this.m_loggerOutput);
         	this.Controls.Add(this.label3);
         	this.Controls.Add(this.port);
-        	this.Controls.Add(this.label1);
         	this.Controls.Add(this.lunchServer);
-        	this.Controls.Add(this.input);
         	this.Name = "Server_Form";
         	this.Text = "Sub Marine Server";
         	this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Server_FormFormClosing);
         	this.ResumeLayout(false);
         	this.PerformLayout();
         }
+        private System.Windows.Forms.ListBox m_loggerOutput;
 
         #endregion
 
-        private System.Windows.Forms.TextBox input;
         private System.Windows.Forms.Button lunchServer;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox port;
         private System.Windows.Forms.Label label3;
     }
