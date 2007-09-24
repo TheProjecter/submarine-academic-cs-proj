0334ab3c03516c2de1aa3e3456fd3fe2dbfce1ae
damagedspline <damagedspline@0286fb24-2438-0410-92b8-11115a6f368b>
2007-09-24 22:28:31 +0000

    Added toolTips for both Client_Form and Server_Form

--- Sub_Marine/Server_Form.Designer.cs
+++ Sub_Marine/Server_Form.Designer.cs
@@ -33,7 +33,7 @@
         	this.port = new System.Windows.Forms.TextBox();
         	this.label3 = new System.Windows.Forms.Label();
         	this.m_loggerOutput = new System.Windows.Forms.ListBox();
-        	this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+        	this.m_tooltip = new System.Windows.Forms.ToolTip(this.components);
         	this.SuspendLayout();
         	// 
         	// lunchServer
@@ -43,6 +43,7 @@
         	this.lunchServer.Size = new System.Drawing.Size(50, 41);
         	this.lunchServer.TabIndex = 1;
         	this.lunchServer.Text = "Lunch Server";
+        	this.m_tooltip.SetToolTip(this.lunchServer, "Launch the server");
         	this.lunchServer.UseVisualStyleBackColor = true;
         	this.lunchServer.Click += new System.EventHandler(this.lunchServer_Click);
         	// 
@@ -52,6 +53,7 @@
         	this.port.Name = "port";
         	this.port.Size = new System.Drawing.Size(138, 20);
         	this.port.TabIndex = 4;
+        	this.m_tooltip.SetToolTip(this.port, "Port number");
         	// 
         	// label3
         	// 
@@ -70,6 +72,7 @@
         	this.m_loggerOutput.Name = "m_loggerOutput";
         	this.m_loggerOutput.Size = new System.Drawing.Size(348, 186);
         	this.m_loggerOutput.TabIndex = 7;
+        	this.m_tooltip.SetToolTip(this.m_loggerOutput, "Logger output");
         	// 
         	// Server_Form
         	// 
@@ -80,13 +83,14 @@
         	this.Controls.Add(this.label3);
         	this.Controls.Add(this.port);
         	this.Controls.Add(this.lunchServer);
+        	this.MaximizeBox = false;
         	this.Name = "Server_Form";
         	this.Text = "Sub Marine Server";
         	this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Server_FormFormClosing);
         	this.ResumeLayout(false);
         	this.PerformLayout();
         }
-        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip m_tooltip;
         private System.Windows.Forms.ListBox m_loggerOutput;
 
         #endregion
